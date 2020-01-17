42a8fe4d03f751abd75657e9f1848418dd88d28a
texdade <davidetessarolo@gmail.com>
2020-01-17 16:46:35 +0100

    CDKeys now returns all games as proper JSON

--- resellers_modules/cdkeys.js
+++ resellers_modules/cdkeys.js
@@ -8,23 +8,31 @@ function getDump() {
     return fetch(url);
 }
 
-//returns the prices of all games in proper formatted json
+//returns the [name, price, availability] of all games in proper formatted json
+//note that CDKeys has a steamID field but it's empty (at least at the time of writing of this code)
 function getPrices(){
     getDump()
         .then(res => res.text()) //res.text returns a promise too
         .then(text => {
             let result = Parser.parse(text); //NOTE: no need to specify delimiters/newlines because autodetects
+            let games = [];
             for(let i=0; i<result.data.length; i++){
-                console.log(result.data[i][1]);
+                //json generation
+                let game = {
+                    name: result.data[i][1], //name
+                    price: result.data[i][4], //price
+                    availability: result.data[i][6] //availability
+                };
+                games.push(game);
             }
+            //TODO: return as promise
+            console.log(JSON.stringify(games));
         }); 
 }
 
 //returns the price of the specified game in proper formatted json
-function getPrice(steamId){
+function getPrice(steamId, gameName){
 
 }
-
-getPrices();
 
 module.exports = {getPrices, getPrice};

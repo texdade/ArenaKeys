f3506ffed089d782dd39ee29eda9ae8cc15c6393
Mouslim Fatnassi <mouslim.fatnassi@studenti.unitn.it>
2020-01-24 16:53:48 +0100

    added homepage section (to be improved)

---
+++ data_template.js
@@ -0,0 +1,32 @@
+var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Cycle",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+$("#selector").autocomplete({
+    source: availableTags
+});
+
+$('#selector').change(function () {
+    alert($('#selector').val());
+});

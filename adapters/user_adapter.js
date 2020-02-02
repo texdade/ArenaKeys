cb1a13d3c79d86e98eb627341c41a9b54c75a386
texdade <davidetessarolo@gmail.com>
2020-02-02 16:48:41 +0100

    Completed routes for users

--- adapters/user_adapter.js
+++ adapters/user_adapter.js
@@ -30,6 +30,42 @@ function createUser(steamId, googleId, name, imageLink, steamProfileUrl, email){
                 let id = {
                     id: userData
                 }
+                resolve(id);
+            } else {
+                reject("400");
+            }
+        }).catch(err => reject(err));
+    });
+}
+
+function updateUser(steamId, googleId, name, imageLink, steamProfileUrl, email){
+    return new Promise((resolve, reject) => {
+        userCRUD.updateUser(steamId, googleId, name, imageLink, steamProfileUrl, email).then(function(){
+            userCRUD.getUser(id).then(userData => {
+                if(userData != undefined){
+                    let jsonUser = {
+                        steamUserId: userData['steamUserId'],
+                        googleUserId: userData["googleUserId"],
+                        name: userData["name"],
+                        imageLink: userData["imageLink"],
+                        id: userData["id"], //internal id for our db
+                        steamProfileUrl: userData["steamProfileUrl"],
+                        email: userData["email"]
+                    }
+                    resolve(jsonUser);
+                } else {
+                    reject("404");
+                }
+            }).catch(err => reject(err));
+        }).catch(err => reject(err));
+    });
+}
+
+function deleteUser(id){
+    return new Promise((resolve, reject) => {
+        userCRUD.deleteUser(id).then(function(){
+            if(userData != undefined){
+                resolve(id);
             } else {
                 reject("400");
             }
--- routes/user_router.js
+++ routes/user_router.js
@@ -44,4 +44,42 @@ router.post('/user', (req, res) => {
     }
 });
 
+router.put('/user', (req, res) => {
+    let googleId = req.user.googleUserId;
+    let steamId = req.user.steamUserId;
+    let name = req.user.name;
+    let imageLink = req.user.imageLink;
+    let steamProfileUrl = req.user.steamProfileUrl;
+    let email = req.user.email;
+
+    if(googleId == undefined){ //section for users logged with steam
+        manageUserData.createUser(steamId, null, name, imageLink, steamProfileUrl, email)
+            .then(userData => res.status(200).json(userData)) //return id of the newly created user
+            .catch(err => res.status(404).json({}));
+    }else if(steamId == undefined){ //section for users logged with google
+        manageUserData.createUser(null, googleId, name, imageLink, steamProfileUrl, email)
+            .then(userData => res.status(200).json(userData)) //return id of the newly created user
+            .catch(err => res.status(404).json({}));
+    }else{ //this should be impossible, but you never know!
+        res.status(403).json({});
+    }
+});
+
+router.delete('/user', (req, res) => {
+    let googleId = req.user.googleUserId;
+    let steamId = req.user.steamUserId;
+
+    if(googleId == undefined){ //section for users logged with steam
+        manageUserData.deleteUser(steamId)
+            .then(userData => res.status(200).json(userData))
+            .catch(err => res.status(404).json({}));
+    }else if(steamId == undefined){ //section for users logged with google
+        manageUserData.deleteUser(googleId)
+            .then(userData => res.status(200).json(userData))
+            .catch(err => res.status(404).json({}));
+    }else{ //this should be impossible, but you never know!
+        res.status(403).json({});
+    }
+});
+
 module.exports = router;

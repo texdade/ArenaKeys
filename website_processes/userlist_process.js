4472ddc49d8a035ccf5160ab9f7baacc465e220c
texdade <davidetessarolo@gmail.com>
2020-02-10 09:58:42 +0100

    Changed URLs for Amazon AWS service

--- website_processes/userlist_process.js
+++ website_processes/userlist_process.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch-npm');
 const utilities = require('../db/utilities');
 
-let apiBaseUrl = "https://gamekeys-arena.herokuapp.com";
+let apiBaseUrl = "https://ec2-34-245-97-253.eu-west-1.compute.amazonaws.com";
 if(process.env.LOCAL)
     apiBaseUrl = "http://localhost:3000";
 
